dd15443ee41a77fe01d515b7752f40bf4ccf1d34
Kejvin Pashaj <kevin.952011@gmail.com>
2023-08-21 15:46:48 +0100

    fixed js

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,18 @@
 const phoneInput = document.getElementById("phone");
 const checkNumberButton = document.querySelector(".check-button");
 const resultWrapper = document.getElementById("result-wrapper");
+let newResult = document.createElement("p");
 
 function telephoneCheck(str) {
-    return (/^(1\s|1)?(\(\d{3}\)|\d{3})(-|\s)?\d{3}(-|\s)?\d{4}$/g).test(str)
+    return /^(1\s|1)?(\(\d{3}\)|\d{3})(-|\s)?\d{3}(-|\s)?\d{4}$/g.test(str)
 }
 
 checkNumberButton.addEventListener("click", function(){
-    let newResult = document.createElement("p");
+    newResult.innerText = ""
     resultWrapper.appendChild(newResult);
     if (!telephoneCheck(phoneInput.value)){
         newResult.innerText = "Fail";
+    } else {
+        newResult.innerText = "Success";
     }
-    newResult.innerText = "Success";
-
-    console.log(phoneInput.value)
 })
